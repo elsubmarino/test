57b0707ab432b12a0a922ce2873fa00a45dc613b
JiwanKim <jisungyeah@gmail.com>
2025-04-20 11:51:50 +0900

    adsfsadfasf

--- index.js
+++ index.js
@@ -33,11 +33,13 @@ app.get("/api", (req, res) => {
 });
 
 app.get("/api/:val", (req, res) => {
-  const { val } = req.params;
+  let { val } = req.params;
   //const pattern = /^(\d{4}-\d{2}-\d{2}$)|(^\d{13}$)/;
   //const result = pattern.exec(val);
   try {
-    if (/^\d{13}$/.test(val)) val = Number(val);
+    if (/^\d{13}$/.test(val)) {
+      val = Number(val);
+    }
     const result = new Date(val);
     if (result instanceof Date && isNaN(result)) {
       res.json({
